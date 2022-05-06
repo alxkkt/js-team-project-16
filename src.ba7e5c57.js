0fdf709249660db811dbe1a31804cfa82ac3fc72
zeddjke <oleksii.kikot@yahoo.com>
2022-05-06 21:26:56 +0000

    Deploying to gh-pages from @ zeddjke/js-team-project-16@fa298b674fbdafb7c80dbe19d80f33c9a5aaca71 🚀

--- src.ba7e5c57.js
+++ src.ba7e5c57.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/js-team-project-16/src.563a5cfd.js.map
+//# sourceMappingURL=/js-team-project-16/src.ba7e5c57.js.map
