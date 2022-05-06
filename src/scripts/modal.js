0acf723ccb4bb4cf968a9eb5104c63994f2f31c7
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-05-06 21:42:18 +0200

    review blyat

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,14 +1,26 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelectorAll('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  modal: document.querySelector('[data-modal]'),
+};
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.gallery.addEventListener('click', openModal);
+refs.modal.addEventListener('click', closeModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+function openModal(e) {
+  console.dir(e.target);
+  if (e.target.parentNode.className !== 'film__item' && e.target.nodeName !== 'P') return;
+
+  refs.modal.classList.remove('is-hidden');
+}
+
+function closeModal(e) {
+  if (e.target !== refs.modal && e.target.nodeName !== 'svg' && e.target.nodeName !== 'use') return;
+
+  document.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      refs.modal.classList.add('is-hidden');
+    }
+  });
+
+  refs.modal.classList.add('is-hidden');
+}
