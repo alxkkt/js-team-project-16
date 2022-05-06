2edb3f13ad66dac7daffc657d5d62e3a9cd1707b
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-05-07 00:18:54 +0200

    added backdrop closing on click

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -2,16 +2,22 @@ const refs = {
   gallery: document.querySelector('.gallery'),
   closeBtn: document.querySelector('[data-modal-close]'),
   modal: document.querySelector('[data-modal]'),
+  backdrop: document.querySelector('.backdrop'),
 };
 
 refs.gallery.addEventListener('click', openModal);
 refs.closeBtn.addEventListener('click', closeModal);
+refs.backdrop.addEventListener('click', onBackdrop);
 
 function openModal(e) {
   if (e.target === refs.gallery) return;
 
   refs.modal.classList.remove('is-hidden');
   window.addEventListener('keydown', onEscPress);
+}
+
+function onBackdrop(e) {
+  e.target === refs.backdrop && closeModal();
 }
 
 function closeModal() {
