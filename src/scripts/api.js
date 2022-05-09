6a3e59754f44c8d1baadf04baa72575f033fef30
iwillywonka94 <iwillywonka94@gmail.com>
2022-05-09 12:31:15 +0300

    Update api.js

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,17 +1,27 @@
 import axios from "axios";
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
-const key = document.querySelector(".gallery")
+const TREND_URL = `/trending/movie/week`;
+const SEARCH_URL = `/search/movie`
 const API_KEY = 'f65bce350427b2684a98ce5b213c02c8';
-axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 
-function getData () {
+async function getTrendFilm (page) {
     try {
-        return axios.get(`trending/movie/week?api_key=${API_KEY}&language=en&page=1`)
+        const { data } = await axios.get(`${TREND_URL}?api_key=${API_KEY}&page=${page}`);
+        return data
     } catch (error) {
         console.error(error);
     }
 }
 
-key.addEventListener("click", (e) => {
-    console.log(getData())
-})
+console.log(getTrendFilm(1));
+
+async function getSearchFilm (value, page) {
+    try {
+        const {data} = await axios.get(`${SEARCH_URL}?api_key=${API_KEY}&query=${value}&page=${page}`);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+console.log(getSearchFilm("Spider", 1))
