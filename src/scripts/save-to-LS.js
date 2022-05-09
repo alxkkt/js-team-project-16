09dbb69644a14eaa95aded9e28f9ef3b29ed1c1f
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-05-09 13:41:42 +0300

    Add buttons on click on "My library"; fix buttons css

--- src/scripts/save-to-LS.js
+++ src/scripts/save-to-LS.js
@@ -1,5 +1,7 @@
 const galleryRef = document.querySelector('.gallery');
 const btn = document.querySelector('.navbar-item_btn');
+const btnContainer = document.querySelector('.btns-container');
+const searchContainer = document.querySelector('.search-form');
 
 // btns-container
 
@@ -9,4 +11,6 @@ function togglePages(e) {
   e.preventDefault();
 
   galleryRef.innerHTML = '<div class=warn>ThereIsNoSpoon</div>';
+  btnContainer.classList.remove('visually-hidden');
+  searchContainer.classList.add('visually-hidden');
 }
