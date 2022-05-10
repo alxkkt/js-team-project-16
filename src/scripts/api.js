88ea56dafdee6c4fcbcea1b61682e8078df22efa
Mrichk0 <zaq4769@gmail.com>
2022-05-10 13:10:13 +0300

    pagination for search

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { pagination } from './pagination';
+
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
 const TREND_URL = `/trending/movie/week`;
@@ -6,6 +8,8 @@ const SEARCH_URL = `/search/movie`;
 const API_KEY = 'f65bce350427b2684a98ce5b213c02c8';
 
 const gallery = document.querySelector('.gallery');
+const input = document.querySelector('.input-box');
+const form = document.querySelector('.search-form');
 // fetch TREND FILM
 async function getTrendFilm(page) {
   try {
@@ -21,10 +25,28 @@ async function getSearchFilm(value, page) {
     const { data } = await axios.get(
       `${SEARCH_URL}?api_key=${API_KEY}&query=${value}&page=${page}`,
     );
+
+    return data;
   } catch (error) {
     console.error(error);
   }
 }
+
+//search submit fun
+form.addEventListener('submit', onButtonClick);
+
+async function onButtonClick(event) {
+  event.preventDefault();
+  const searchText = input.value.trim();
+  if (searchText !== '') {
+    const data = await getSearchFilm(searchText, 1);
+    console.log('serch date', data);
+    renderGallery(data.results);
+  } else if (data.results.length > 20) {
+    searchPagination(totalResults);
+  }
+}
+
 // fetch GENRE
 async function getGenre() {
   try {
@@ -40,6 +62,7 @@ async function firstPage() {
     const data = await getTrendFilm(1);
     const movies = data.results;
     renderGallery(movies);
+    trendPagination(data.total_results);
   } catch (error) {
     console.error(error);
   }
@@ -51,6 +74,37 @@ function createYear(obj) {
   return obj.slice(0, 4);
 }
 // MARK UP
+
+//SEARCH PAGINATION
+function searchPagination(totalResults) {
+  const totalPagination = pagination(totalResults);
+  totalPagination.on('beforeMove', async event => {
+    const data = await getSearchFilm(value, event.page);
+    window.scrollTo(0, 0);
+    renderGallery(data.results);
+  });
+}
+
+//trend pagination
+function trendPagination(totalResults) {
+  const totalPagination = pagination(totalResults);
+  totalPagination.on('beforeMove', async event => {
+    const data = await getTrendFilm(event.page);
+    window.scrollTo(0, 0);
+    renderGallery(data.results);
+  });
+}
+
+function getGenreName(genreId) {
+  const genreNames = genreId.map(id => {
+    const { name } = genres.find(item => item.id === id) ? genres.find(item => item.id === id) : '';
+    return name;
+  });
+  return genreNames;
+}
+
+// getGenreName();
+
 function renderGallery(movies) {
   const markUp = movies
     .map(
@@ -77,5 +131,5 @@ function renderGallery(movies) {
     `,
     )
     .join('');
-  gallery.insertAdjacentHTML('beforeend', markUp);
+  gallery.innerHTML = markUp;
 }
